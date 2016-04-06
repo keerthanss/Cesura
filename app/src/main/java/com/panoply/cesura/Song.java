cdc208aff0e2afdd1f8571d22e5e3a261a6ee899
Keerthan Shagrithaya <keerthanss9@gmail.com>
2016-04-07 03:05:55 +0530

    Updated time since last play

--- app/src/main/java/com/panoply/cesura/Song.java
+++ app/src/main/java/com/panoply/cesura/Song.java
@@ -1,6 +1,9 @@
 package com.panoply.cesura;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Song {
     private long id;
@@ -10,11 +13,56 @@ public class Song {
     private String genre;
     private int playCount;
     private int timeSinceLastPlay;
+    private String lastPlayTimeStamp;
+
+    private class DateTime{
+        private int year;
+        private int month;
+        private int day;
+        private int hour;
+        private int minute;
+        private int second;
+
+        public DateTime(String date){
+            year = Integer.valueOf(date.substring(0, 3));
+            month = Integer.valueOf(date.substring(5, 6));
+            day = Integer.valueOf(date.substring(8, 9));
+            hour = Integer.valueOf(date.substring(11,12));
+            minute = Integer.valueOf(date.substring(14,15));
+            second = Integer.valueOf(date.substring(17,18));
+        }
+
+        public int compareTo(DateTime d){
+            int result=0;
+            if(year != d.year){
+                result = (year - d.year)*24*365;
+            }
+            else{
+                if(month!=d.month){
+                    result = (month - d.month)*30*24;
+                }
+                else{
+                    if(day != d.day){
+                        result = (day - d.day)*24;
+                    }
+                    else{
+                        result = hour - d.hour;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
 
     public Song(String artist, long id, String title) {
         this.artist = artist;
         this.id = id;
         this.title = title;
+    }
+
+    public void markTimeStamp(){
+        lastPlayTimeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
     }
 
     public String getArtist() {
@@ -42,6 +90,10 @@ public class Song {
     }
 
     public int getLastPlay() {
+        String currentTimeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
+        DateTime old = new DateTime(lastPlayTimeStamp);
+        DateTime current = new DateTime(currentTimeStamp);
+        timeSinceLastPlay = current.compareTo(old);
         return timeSinceLastPlay;
     }
 }
